5d7da7964e891f68fe0ada03403b6e531d1d3c67
jhbdream <1023506846@qq.com>
2019-05-06 18:56:48 +0800

    Add a port file

---
+++ port/touch_device.c
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-02-08     Zhangyihong  the first version
+ * 2019-05-06     jhb          Adaptation package
+ */
+
+#include <rtthread.h>
+#include <rthw.h>
+#include <rtdevice.h>
+#include "touch.h"
+#include <string.h>
+
+#ifdef PKG_USING_TOUCH
+
+/*Enable debug information*/
+#define DBG_ENABLE
+
+#ifdef  DBG_ENABLE
+#define DBG_SECTION_NAME  "TOUCH.ft"
+#define DBG_LEVEL         DBG_LOG
+#define DBG_COLOR
+#include <rtdbg.h>
+#endif
+
+#define IIC_RETRY_NUM 2
+#define TOUCH_INT_PIN 0
+#define CHIP_ID_REG                 0x00U
+#define CHIP_ID_VALUE               0x00U
+#define TOUCH_SLAVE_ADDR            0x00U
+
+static struct rt_i2c_bus_device *i2c_bus;
+static struct touch_drivers driver;
+
+static int read(struct rt_i2c_bus_device *i2c_bus, rt_uint8_t addr, rt_uint8_t *buffer, rt_size_t length)
+{
+    int ret = -1;
+    int retries = 0;
+
+    struct rt_i2c_msg msgs[] =
+    {
+        {
+            .addr   = TOUCH_SLAVE_ADDR,
+            .flags  = RT_I2C_WR,
+            .len    = 1,
+            .buf    = &addr,
+        },
+        {
+            .addr   = TOUCH_SLAVE_ADDR,
+            .flags  = RT_I2C_RD,
+            .len    = length,
+            .buf    = buffer,
+        },
+    };
+
+    while (retries < IIC_RETRY_NUM)
+    {
+        ret = rt_i2c_transfer(i2c_bus, msgs, 2);
+        if (ret == 2)break;
+        retries++;
+    }
+
+    if (retries >= IIC_RETRY_NUM)
+    {
+#ifdef DBG_ENABLE
+        LOG_E("%s i2c read error: %d", __func__, ret);
+#endif
+        return -1;
+    }
+
+    return ret;
+}
+
+static void write(touch_drv_t driver, struct rt_i2c_bus_device *i2c_bus, rt_uint8_t addr, rt_uint8_t *buffer, rt_size_t length)
+{
+
+    rt_uint8_t *send_buffer = rt_malloc(length + 1);
+
+    RT_ASSERT(send_buffer);
+
+    send_buffer[0] = addr;
+    memcpy(send_buffer + 1, buffer, length);
+
+    struct rt_i2c_msg msgs[] =
+    {
+        {
+            .addr   = TOUCH_SLAVE_ADDR,
+            .flags  = RT_I2C_WR,
+            .len    = length + 1,
+            .buf    = send_buffer,
+        }
+    };
+
+    length = i2c_transfer(i2c_bus, msgs, 1);
+    rt_free(send_buffer);
+    send_buffer = RT_NULL;
+}
+
+static void isr_enable(rt_bool_t enable)
+{
+    rt_pin_irq_enable(TOUCH_INT_PIN, enable);
+}
+
+static void touch_isr(void *parameter)
+{
+    isr_enable(RT_FALSE);
+    rt_touch_release_sem();
+}
+
+static rt_err_t read_point(touch_msg_t msg)
+{
+    int ret = -1;
+    uint8_t point_num = 0;
+    static uint8_t s_tp_down = 0;
+    uint8_t point[6];
+    ret = read(i2c_bus, 0x02, &point_num, 1);
+    if (ret < 0)
+    {
+        return RT_ERROR;
+    }
+    
+    if (point_num == 0)
+    {
+        if (s_tp_down)
+        {
+            s_tp_down = 0;
+            msg->event = TOUCH_EVENT_UP;
+            return RT_EOK;
+        }
+        msg->event = TOUCH_EVENT_NONE;
+        return RT_ERROR;
+    }
+    
+    ret = read(i2c_bus, 0x03, point, 6);
+    if (ret < 0)
+    {
+        return RT_ERROR;
+    }
+
+    msg->x = (point[0]&0x0F) << 8 | point[1];
+    msg->y = (point[2]&0x0F) << 8 | point[3];
+    if (s_tp_down)
+    {
+        msg->event = TOUCH_EVENT_MOVE;
+        return RT_EOK;
+    }
+    msg->event = TOUCH_EVENT_DOWN;
+    s_tp_down = 1;
+    
+    return RT_EOK;
+}
+
+static void init(struct rt_i2c_bus_device *i2c_bus)
+{
+    if (i2c_bus == RT_NULL)
+    {
+        i2c_bus = i2c_bus;
+    }
+
+    rt_pin_mode(TOUCH_INT_PIN, PIN_MODE_INPUT_PULLUP);
+    rt_pin_attach_irq(TOUCH_INT_PIN, PIN_IRQ_MODE_FALLING, touch_isr, RT_NULL);
+
+    rt_thread_mdelay(200);
+}
+
+static void deinit(void)
+{
+    
+}
+
+struct touch_ops ops =
+{
+    isr_enable,
+    read_point,
+    init,
+    deinit,
+};
+
+static rt_bool_t probe(struct rt_i2c_bus_device *i2c_bus)
+{
+    int err = 0;
+    uint8_t cid = 0xFF;
+
+    i2c_bus = i2c_bus;
+    err = read(i2c_bus, CHIP_ID_REG, (uint8_t *)&cid, 1);
+    if (err < 0)
+    {
+#ifdef DBG_ENABLE
+        LOG_E("%s failed: %d", __func__, err);
+#endif     
+        return RT_FALSE;
+    }  
+
+#ifdef DBG_ENABLE
+    LOG_I("touch CID:%02X", cid);
+#endif 
+   
+    if(cid == CHIP_ID_VALUE)
+    {
+        return RT_TRUE;
+    }
+    return RT_FALSE;
+}
+
+int driver_register(void)
+{
+    driver.probe = probe;
+    driver.ops = &ops;
+    driver.user_data = RT_NULL;
+    rt_touch_drivers_register(&driver);
+    return 0;
+}
+
+INIT_DEVICE_EXPORT(driver_register);
+
+#endif
